4ea81f1a899cc4be786b8103c1a84e1e2df6de08
Zeppcvet <84909157+Zeppcvet@users.noreply.github.com>
2021-05-28 19:45:53 +0300

    Remove steps with root

--- Jira.sh
+++ Jira.sh
@@ -3,9 +3,7 @@ sudo apt-get update
 sudo apt-get install default-jdk
 wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-core-8.13.7-x64.bin
 chmod +x atlassian-jira-core-8.13.7-x64.bin
-sudo su
 printf "o\2\/ops/atlassian/jira\/var/atlassian/application-data/jira\1\y\i\y" | ./atlassian-jira-core-8.13.7-x64.bin
-exit
 sudo apt install ufw
 ufw allow 8181
 ufw allow 8005
